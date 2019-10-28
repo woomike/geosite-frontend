3a380c49f8a9ec078346c39e09fec0a64dfc3ffd
woomike <mike.woo@atomrain.com>
2019-10-27 19:33:29 -0700

    [wip] add map markers, add hard coded places lived / worked, add basic styling to main page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Geosite Frontend
+        Areas of Interest
       </header>
       <div className="main-container">
         <Navigation />
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
 import GoogleMapReact from 'google-map-react'
+import MapMarkerPNG from './map-marker.png'
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const MapMarker= () => {
+  const style = {width: '25px', height: '25px'}
+  return (
+    <img style={style} src={MapMarkerPNG} alt={'Map Marker'} />
+  )
+}
 
 class Map extends Component {
   static defaultProps = {
@@ -12,14 +18,43 @@ class Map extends Component {
     zoom: 11
   };
 
-          // bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
   render() {
     return (
-      <div style={{height: '500px', width: '500px' }}>
+      <div style={{height: '90vh', width: '70%' }}>
         <GoogleMapReact
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
+        <MapMarker
+          lat={34.040857}
+          lng={-118.444013}
+          text={'West Los Angeles'}
+        />
+        <MapMarker
+          lat={37.442788}
+          lng={-122.139112}
+          text={'Palo Alto'}
+        />
+        <MapMarker
+          lat={34.061353}
+          lng={-118.118601}
+          text={'Monterey Park'}
+        />
+        <MapMarker
+          lat={34.021233}
+          lng={-118.481437}
+          text={'Atom Rain'}
+        />
+        <MapMarker
+          lat={33.972629}
+          lng={-118.449782}
+          text={'Wildebeest'}
+        />
+        <MapMarker
+          lat={34.161795}
+          lng={-118.448313}
+          text={'Sanguine Biosciences'}
+        />
         </GoogleMapReact>
       </div>
     );
